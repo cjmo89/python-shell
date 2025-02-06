a65672dcf855071c42100296cf98d9486b620991
Carlos Manrique Ortega <cjmortega@gmail.com>
2025-02-06 05:06:39 +0100

    implement stage 6

--- app/main.py
+++ app/main.py
@@ -10,11 +10,23 @@ def main():
         command = strings[0]
         match command:
             case "exit":
-                sys.exit(int(strings[1]))  # exit returning the first arg
+                if len(strings) > 1:
+                    sys.exit(int(strings[1]))  # exit returning the first arg
+                sys.exit()
             case "echo":
                 print(inString[5:])
+            case "type":
+                print(type(strings[1]))
             case _:
                 print(f"{command}: command not found")
+
+
+def type(arg):
+    builtins = ["echo", "exit"]
+    if arg in builtins:
+        return f"{arg} is a shell builtin"
+    else:
+        return f"{arg}: not found"
 
 
 if __name__ == "__main__":
