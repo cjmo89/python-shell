772cf415ce8ae146aebd52d48c919c058c96ffee
Carlos Manrique Ortega <cjmortega@gmail.com>
2025-02-06 04:45:38 +0100

    ammend stage 4

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
         sys.stdout.write("$ ")
         # Wait for user input
         command = input()
-        if command == "exit":
+        if command == "exit 0":
             sys.exit(0)
         print(f"{command}: command not found")
 
